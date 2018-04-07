f56aee9767ff69c39106f3102cbdf610154f568e
Olivier GODARD <olivier.godard@ensimag.imag.fr>
2018-04-08 00:32:40 +0200

    More pep8 compliant

--- main.py
+++ main.py
@@ -5,207 +5,211 @@ import sys
 import operator
 import os
 
-def hexStrToByteArray(s):
-  ba = bytearray()
-  nbBytes = len(s)//2 - 2 + 1
-  bInt = int(s, 16)
-  ba.extend(bInt.to_bytes(nbBytes, byteorder='big'))
-  return ba
+
+def hex_str_to_bytearray(s):
+    ba = bytearray()
+    nb_bytes = (len(s) // 2) - 2 + 1
+    bInt = int(s, 16)
+    ba.extend(bInt.to_bytes(nb_bytes, byteorder='big'))
+    return ba
 
 
-def hexStrToB64Str(hexStr):
-  decodedStr = codecs.decode(hexStr, 'hex')
-  b64 = codecs.encode(decodedStr,'base64')
+def hex_str_to_b64_str(hex_str):
+    decoded_str = codecs.decode(hex_str, 'hex')
+    b64 = codecs.encode(decoded_str, 'base64')
 
-  return codecs.decode(b64).rstrip()
-
-
-def rateAlphaNumSpace(ba):
-  nbAlphaNumSpace = 0
-  lenBa = len(ba)
-
-  for i in ba :
-    if i == 0x20 or (ord('a') <= i <= ord('z')) or (ord('A') <= i <= ord('Z')):
-      nbAlphaNumSpace +=1
-
-  return nbAlphaNumSpace/lenBa
-
-def isPlainText(ba, rate = 0.90):
-  if (rateAlphaNumSpace(ba) >= rate):
-    return True
-  return False
+    return codecs.decode(b64).rstrip()
 
 
-def xorBuffer(b1, b2):
-  if len(b1) != len(b2) :
-    raise ValueError("bytearrays are not the same length")
-  
-  b3 = bytearray(len(b1))
-  
-  for i in range(0, len(b1)) :
-    b3[i] = b2[i] ^ b1[i]
-  
-  return b3
+def rate_alpha_num_space(ba):
+    nb_alpha_num_space = 0
+    len_ba = len(ba)
+
+    for i in ba:
+        if i == (0x20 or (ord('a') <= i <= ord('z'))
+                 or (ord('A') <= i <= ord('Z'))):
+            nb_alpha_num_space += 1
+
+    return nb_alpha_num_space / len_ba
 
 
-def xorKey(dataIn, key):
-  """
-  Return xored data.
-
-  Args:
-    dataIn bytearay: data to decode
-    key bytearray: key used to decode
-  Returns:
-    bytearray: dataIn ^ key
-  """
-  key_len = len(key)
-  dataOut = bytearray(len(dataIn))
-  for i in range(0, len(dataIn)):
-    dataOut[i] = dataIn[i] ^ key[i%key_len]
-  return dataOut
+def is_plain_text(ba, rate=0.90):
+    if (rate_alpha_num_space(ba) >= rate):
+        return True
+    return False
 
 
-def countByteOccurence(ba):
-  """
-  Return sorted list of tuple.
-
-  Args:
-    ba bytearray: Ciphered text
-  Returns:
-    list: a sorted list of 256 tuples (char, %presence)
-  """
-  tab = [0] * 256
-  strlen = len(ba)
-
-  for i in ba:
-    tab[i] = tab[i] + 1/strlen
-
-  sortedBytes = list(zip(range(0, 256), tab))
-  sortedBytes.sort(key=operator.itemgetter(1))
-  
-  return sortedBytes
+def xor_buffer(b1, b2):
+    if len(b1) != len(b2):
+        raise ValueError("bytearrays are not the same length")
+    b3 = bytearray(len(b1))
+    for i in range(0, len(b1)):
+        b3[i] = b2[i] ^ b1[i]
+    return b3
 
 
-def displayCharOccurences(sortedOcc) :
-  for i in reversed(sortedOcc) :
-    if i[1] == 0 :
-      break
-    print(hex(i[0]), ":", i[1])
+def xor_key(data_in, key):
+    """
+    Return xored data.
+
+    Args:
+        data_in bytearay: data to decode
+        key bytearray: key used to decode
+    Returns:
+        bytearray: data_in ^ key
+    """
+    key_len = len(key)
+    dataOut = bytearray(len(data_in))
+    for i in range(0, len(data_in)):
+        dataOut[i] = data_in[i] ^ key[i % key_len]
+    return dataOut
 
 
-def hammingDistance(a,b):
-  """
-  Return the hamming distance of patterns a and b.
-  a and b must have the same length.
+def count_byte_occurence(ba):
+    """
+    Return sorted list of tuple.
 
-  Args:
-    a bytearray: first pattern
-    b bytearray: second pattern
-  Returns:
-    int: the hamming distance of a and b or -1 if it can't 
-    be computed
-  """
-  if (len(a)!=len(b) or len(a) == 0) :
-    return -1
-  p1XorP2 = xorBuffer(a, b)
-  intP1XorP2 = int.from_bytes(p1XorP2, byteorder='big', signed=False)
-  result = bin(intP1XorP2).count("1")
-  return result
+    Args:
+        ba bytearray: Ciphered text
+    Returns:
+        list: a sorted list of 256 tuples (char, %presence)
+    """
+    tab = [0] * 256
+    strlen = len(ba)
 
+    for i in ba:
+        tab[i] = tab[i] + (1 / strlen)
 
-def readB64File(path):
-  """
-  Read file which encodes data in b64 on several lines.
+    sortedBytes = list(zip(range(0, 256), tab))
+    sortedBytes.sort(key=operator.itemgetter(1))
 
-  Args:
-    path string: path to file
-  Returns:
-    bytearray: data decoded from message in the given file 
-  """
-  try :
-    file = open(path, 'r')
-  except:
-    print("Can't open file " + path + ".")
-    return None
-  stringFile = str()
-  for line in file:
-    lineWithoutReturn = line.rstrip()
-    stringFile += lineWithoutReturn
-  res = base64.b64decode(stringFile)
-  file.close()
-  return res
+    return sortedBytes
 
 
-def splitByteArray(ba, nbBa):
-  """
-  Split a bytearray into several bytearrays.
-  The bytes are split in round robin between the output bytearrays.
+def display_char_occurences(sortedOcc):
+    for i in reversed(sortedOcc):
+        if i[1] == 0:
+            break
+        print(hex(i[0]), ":", i[1])
 
-  Args:
-    ba bytearray: bytearray to split
-    nbBa int: number of bytearray
-  Returns:
-    list: list of nbBa bytearray with all original bytes
-  """
-  tab = list()
-  lenBa = len(ba)
 
-  # create sub bytearray with appropirate len
-  rest = lenBa % nbBa
-  for i in range(0,nbBa):
-    lenSubBa = lenBa//nbBa
-    if rest - i > 0 :
-      lenSubBa += 1
-    tab.append(bytearray(lenSubBa))
+def hamming_distance(a, b):
+    """
+    Return the hamming distance of patterns a and b.
+    a and b must have the same length.
 
-  # dispatch bytes
-  for i in range(0, len(ba)):
-    baIndex = i % nbBa
-    tab[baIndex][i//nbBa] = (ba[i])
+    Args:
+        a bytearray: first pattern
+        b bytearray: second pattern
+    Returns:
+        int: the hamming distance of a and b or -1 if it can't
+        be computed
+    """
+    if (len(a) != len(b) or len(a) == 0):
+        return -1
+    p1_xor_p2 = xor_buffer(a, b)
+    int_p1_xor_p2 = int.from_bytes(p1_xor_p2, byteorder='big', signed=False)
+    result = bin(int_p1_xor_p2).count("1")
+    return result
 
-  return tab
 
-def guessKeyLenHD(ba, maxLen):
-  hammingDistances = [0] * maxLen
+def read_b64_file(path):
+    """
+    Read file which encodes data in b64 on several lines.
 
-  for i in range(1, maxLen+1):
-    nb_hd = 20
-    for j in range(0, nb_hd) :
-      ba1 = ba[(2*j)*i:(2*j+1)*i]
-      ba2 = ba[(2*j+1)*i:(2*j+2)*i]
-      hammingDistances[i-1] += hammingDistance(ba1,ba2)/i
-    hammingDistances[i-1] = hammingDistances[i-1] / nb_hd
+    Args:
+        path string: path to file
+    Returns:
+        bytearray: data decoded from message in the given file
+    """
+    try:
+        file = open(path, 'r')
+    except (OSError, IOError) as e:
+        print("Can't open file " + path + "(" + e.errno + ")" + ".")
+        return None
+    string_file = str()
+    for line in file:
+        lineWithoutReturn = line.rstrip()
+        string_file += lineWithoutReturn
+    res = base64.b64decode(string_file)
+    file.close()
+    return res
 
-  sortedHD = list(zip(range(1, maxLen+1), hammingDistances))
-  sortedHD.sort(key=operator.itemgetter(1))
-  return(sortedHD[0][0])
+
+def split_bytearray(ba, nb_ba):
+    """
+    Split a bytearray into several bytearrays.
+    The bytes are split in round robin between the output bytearrays.
+
+    Args:
+      ba bytearray: bytearray to split
+      nb_ba int: number of bytearray
+    Returns:
+      list: list of nb_ba bytearray with all original bytes
+    """
+    tab = list()
+    len_ba = len(ba)
+
+    # create sub bytearray with appropirate len
+    rest = len_ba % nb_ba
+    for i in range(0, nb_ba):
+        len_sub_ba = len_ba // nb_ba
+        if rest - i > 0:
+            len_sub_ba += 1
+        tab.append(bytearray(len_sub_ba))
+
+    # dispatch bytes
+    for i in range(0, len(ba)):
+        ba_index = i % nb_ba
+        tab[ba_index][i // nb_ba] = (ba[i])
+
+    return tab
+
+
+def guess_key_len_HD(ba, max_len):
+    hamming_distances = [0] * max_len
+
+    for i in range(1, max_len + 1):
+        nb_hd = 20
+        for j in range(0, nb_hd):
+            ba1 = ba[(2 * j) * i:(2 * j + 1) * i]
+            ba2 = ba[(2 * j + 1) * i:(2 * j + 2) * i]
+            hamming_distances[i - 1] += hamming_distance(ba1, ba2) / i
+        hamming_distances[i - 1] = hamming_distances[i - 1] / nb_hd
+
+    sorted_HD = list(zip(range(1, max_len + 1), hamming_distances))
+    sorted_HD.sort(key=operator.itemgetter(1))
+    return(sorted_HD[0][0])
+
 
 def decrypt_xor_text(cipher_text):
-  secret_key = bytearray()
+    secret_key = bytearray()
 
-  key_len = guessKeyLenHD(cipher_text, 42)
-  print("best key length using hamming distance method :", key_len)
-  subText = splitByteArray(cipher_text, key_len)
- 
-  print("display of the most common bytes of each sets")
-  listByteOcc = list()
-  for i in subText:
-    listByteOcc.append(countByteOccurence(i)[250:256])
-  for index, i in enumerate(listByteOcc):
-    print("###", index)
-    displayCharOccurences(i)
+    key_len = guess_key_len_HD(cipher_text, 42)
+    print("best key length using hamming distance method:", key_len)
+    subtext = split_bytearray(cipher_text, key_len)
 
-  for i in listByteOcc:
-    most_present_byte = i[-1][0]
-    char_key = most_present_byte^0x20
-    secret_key.append(char_key)
-    print("xoring " + hex(most_present_byte) + " and 0x20 : " , chr(char_key), hex(char_key))
+    print("display of the most common bytes of each sets")
+    byte_occ_list = list()
+    for i in subtext:
+        byte_occ_list.append(count_byte_occurence(i)[250:256])
+    for index, i in enumerate(byte_occ_list):
+        print("###", index)
+        display_char_occurences(i)
 
-  print("The secrekt key is : " + 'secret_key.decode("ascii")')
-  print("The plain text is :")
-  plain_text = xorKey(cipher_text, secret_key)
-  print(plain_text.decode("utf-8"))
-  return plain_text
+    for i in byte_occ_list:
+        most_present_byte = i[-1][0]
+        char_key = most_present_byte ^ 0x20
+        secret_key.append(char_key)
+        print("xoring " + hex(most_present_byte) + " and 0x20: ",
+              chr(char_key), hex(char_key))
 
-if __name__ == "__main__" :
-  pass
+    print("The secrekt key is: " + 'secret_key.decode("ascii")')
+    print("The plain text is:")
+    plain_text = xor_key(cipher_text, secret_key)
+    print(plain_text.decode("utf-8"))
+    return plain_text
+
+
+if __name__ == "__main__":
+    pass
--- tests.py
+++ tests.py
@@ -3,136 +3,133 @@
 import unittest
 from main import *
 import os
-  
-class MyTests(unittest.TestCase) :
-  
-  def testB64(self) :
-    print("Testing hexStrToB64Str()...")
-    inputStr = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    expectedStr = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    self.assertEqual(expectedStr, hexStrToB64Str(inputStr))
 
 
-  def testHexStrToByteArray(self) :
-    print("Testing hexStrToByteArray()...")
-    cipherTextStr = "0x1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    cipheredText = hexStrToByteArray(cipherTextStr)
-    self.assertEqual(bytearray(b'\x1b77316?x\x15\x1b\x7f+x413=x9x(7-6<x7>x:9;76'), cipheredText)
-  
+class MyTests(unittest.TestCase):
 
-  def testXorBuffer(self) :
-    print("Testing XorBuffer()...")
-    b1 = bytearray()
-    b1int = 0x1c0111001f010100061a024b53535009181c
-    b1.extend(b1int.to_bytes(20, byteorder='big'))
-    b2 = bytearray()
-    b2int = 0x686974207468652062756c6c277320657965
-    b2.extend(b2int.to_bytes(20, byteorder='big'))
-    b3 = bytearray()
-    b3int = 0x746865206b696420646f6e277420706c6179
-    b3.extend(b3int.to_bytes(20, byteorder='big'))
-    self.assertEqual(b3, xorBuffer(b1,b2))
+    def test_b64(self):
+        print("Testing hex_str_to_b64_str()...")
+        input_str = ("49276d206b696c6c696e6720796f757220627261696e206c696b6520"
+                     "6120706f69736f6e6f7573206d757368726f6f6d")
+        expected_str = ("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3Vz"
+                        "IG11c2hyb29t")
+        self.assertEqual(expected_str, hex_str_to_b64_str(input_str))
+
+    def test_hex_str_to_bytearray(self):
+        print("Testing hex_str_to_bytearray()...")
+        cipher_text_str = ("0x1b37373331363f78151b7f2b783431333d78397828372d36"
+                           "3c78373e783a393b3736")
+        cipher_text = hex_str_to_bytearray(cipher_text_str)
+        self.assertEqual(bytearray(b'\x1b77316?x\x15\x1b\x7f+x413=x9x(7-6<x7>x:9;76'), cipher_text)
+
+    def test_xor_buffer(self):
+        print("Testing XorBuffer()...")
+        b1 = bytearray()
+        b1int = 0x1c0111001f010100061a024b53535009181c
+        b1.extend(b1int.to_bytes(20, byteorder='big'))
+        b2 = bytearray()
+        b2int = 0x686974207468652062756c6c277320657965
+        b2.extend(b2int.to_bytes(20, byteorder='big'))
+        b3 = bytearray()
+        b3int = 0x746865206b696420646f6e277420706c6179
+        b3.extend(b3int.to_bytes(20, byteorder='big'))
+        self.assertEqual(b3, xor_buffer(b1, b2))
+
+    def test_finding_one_byte_key(self):
+        print("Testing Xoring decription...")
+        cipher_text_str = "0x1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+        cipher_text = hex_str_to_bytearray(cipher_text_str)
+        print("Cipher text is:", cipher_text.hex())
+        res = count_byte_occurence(cipher_text)
+        display_char_occurences(res)
+        self.assertEqual(res[255][0], 0x78)
+        for i in range(1, 237):
+            self.assertEqual(res[i][1], 0.0)
+        print("e is the most common char in english and we know the key is only one byte long.")
+        print("Space is also very common.")
+        print("Good candidates for keys are 'e', 'E' and ' '.")
+        print("The most present char is:", hex(res[255][0]))
+
+        def try_key(char):
+            likely_key_int = res[255][0] ^ ord(char)
+            likely_key = bytearray()
+            likely_key.extend(likely_key_int.to_bytes(1, byteorder='big'))
+            print("trying key", hex(likely_key_int), "deduced assuming the most presnt char is", char + ".")
+            print("xoring", hex(likely_key_int), "and cipher_text...")
+            plain_text = xor_key(cipher_text, likely_key)
+            if is_plain_text(plain_text):
+                print("The message seems readable. The key seems to be ", hex(likely_key_int) + ".")
+                print("Plain Message: ", str(plain_text))
+            else:
+                print("The message seems not readable. The key seems not to be ", hex(likely_key_int) + ".")
+            return plain_text
+
+        try_key('e')
+        try_key('E')
+        solution = try_key(' ')
+
+        solution_expected = bytearray(b"Cooking MC\'s like a pound of bacon")
+        self.assertEqual(solution_expected, solution)
+
+    def test_xor_encription(self):
+        print("Testing Xoring Encription...")
+        plain_text_str = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+        key_str = "ICE"
+        plain_text = bytearray(plain_text_str, "ascii")
+        key = bytearray(key_str, "ascii")
+        expected_result = ("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
+                           "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
+        print("Xoring plain text:\n" + plain_text_str + "\nwith key " + key_str + ".")
+        cipher_text = xor_key(plain_text, key).hex()
+        print("output: " + cipher_text)
+        self.assertEqual(expected_result, cipher_text)
+
+    def test_hamming_distance(self):
+        print("Testing Hamming distance computation...")
+        pattern1_str = "this is a test"
+        pattern2_str = "wokka wokka!!!"
+        pattern1 = bytearray(pattern1_str, "ascii")
+        pattern2 = bytearray(pattern2_str, "ascii")
+        expected_result = 37
+        print("Computing hamming distance between", pattern1_str, "and", pattern2_str + ":")
+        result = hamming_distance(pattern1, pattern2)
+        print("Hamming distance is " + str(result) + ".")
+        self.assertEqual(expected_result, result)
+
+    def test_read_b64_file(self):
+        print("Testing read_b64_file()...")
+        path_to_file = os.path.dirname(os.path.realpath(__file__)) + "/set1/testReadB64.txt"
+        ba = read_b64_file(path_to_file)
+        print(ba)
+        expectedRes = "abcdefhH87"
+        self.assertEqual(expectedRes, ba.decode("ascii"))
+
+    def test_split_bytearray(self):
+        print("Testing test_split_bytearray()...")
+        ba = hex_str_to_bytearray("0xffaabb11cc99ee88dd")
+        expected_res_1 = [bytearray(b'\xff\xbb\xcc\xee\xdd'), bytearray(b'\xaa\x11\x99\x88')]
+        expected_res_2 = [bytearray(b'\xff\x11\xee'), bytearray(b'\xaa\xcc\x88'), bytearray(b'\xbb\x99\xdd')]
+        res1 = split_bytearray(ba, 2)
+        res2 = split_bytearray(ba, 3)
+        self.assertEqual(ba, split_bytearray(ba, 1)[0])
+        self.assertEqual(expected_res_1, res1)
+        self.assertEqual(expected_res_2, res2)
+
+    def test_decrypt_xor_text(self):
+        print(">>> Resolving set1/challenge6.txt")
+        path_to_challenge6 = os.path.dirname(os.path.realpath(__file__)) + "/set1/challenge6.txt"
+        cipher_text = read_b64_file(path_to_challenge6)
+        plain_text = decrypt_xor_text(cipher_text)
+        self.assertEqual("I'm back and", plain_text[0:12].decode("utf-8"))
+
+        print(">>> Resolving set1/my_plain_b64.txt")
+        path_to_my_plain = os.path.dirname(os.path.realpath(__file__)) + "/set1/my_plain_b64.txt"
+        my_key = bytearray("goodd", "ascii")
+        plain_text = read_b64_file(path_to_my_plain)
+        cipher_text = xor_key(plain_text, my_key)
+        plain_text_back = decrypt_xor_text(cipher_text)
+        self.assertEqual(plain_text, plain_text_back)
 
 
-  def testFindingOneByteKey(self):
-    print("Testing Xoring decription...")
-    cipherTextStr = "0x1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    cipheredText = hexStrToByteArray(cipherTextStr)
-    print("Cipher text is :", cipheredText.hex())
-    res = countByteOccurence(cipheredText)
-    displayCharOccurences(res)
-    self.assertEqual(res[255][0], 0x78)
-    for i in range(1, 237):
-      self.assertEqual(res[i][1], 0.0)
-    print("e is the most common char in english and we know the key is only one byte long.")
-    print("Space is also very common.")
-    print("Good candidates for keys are 'e', 'E' and ' '.")
-    print("The most present char is :", hex(res[255][0]))
-
-    def tryKey(ciphBa, char):
-      likelyKeyInt = res[255][0] ^ ord(char)
-      likelyKey = bytearray()
-      likelyKey.extend(likelyKeyInt.to_bytes(1, byteorder='big'))
-      print("trying key", hex(likelyKeyInt), "deduced assuming the most presnt char is", char + ".")
-      print("xoring", hex(likelyKeyInt), "and cipheredText...")
-      plainText = xorKey(cipheredText, likelyKey)
-      if isPlainText(plainText) :
-        print("The message seems readable. The key seems to be ", hex(likelyKeyInt) + ".")
-        print("Plain Message : ", str(plainText))
-      else :
-        print("The message seems not readable. The key seems not to be ", hex(likelyKeyInt) + ".")
-      return plainText
-
-    tryKey(cipheredText, 'e')
-    tryKey(cipheredText, 'E')
-    solution = tryKey(cipheredText, ' ')
-
-    solutionExpected = bytearray(b"Cooking MC\'s like a pound of bacon")
-    self.assertEqual(solutionExpected, solution)
-
-
-  def testXorEncription(self) :
-    print("Testing Xoring Encription...")
-    plainTextStr = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-    keyStr = "ICE"
-    plainText = bytearray(plainTextStr, "ascii")
-    key = bytearray(keyStr, "ascii")
-    expectedResult = ("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
-                      "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
-    print("Xoring plain text :\n" + plainTextStr +"\nwith key " + keyStr + ".")
-    cipherText = xorKey(plainText, key).hex()
-    print("output : " + cipherText)
-    self.assertEqual(expectedResult, cipherText)
-
-
-  def testHammingDistance(self):
-    print("Testing Hamming distance computation...")
-    pattern1Str = "this is a test"
-    pattern2Str = "wokka wokka!!!"
-    pattern1 = bytearray(pattern1Str, "ascii")
-    pattern2 = bytearray(pattern2Str, "ascii")
-    expectedResult = 37
-    print("Computing hamming distance between", pattern1Str, "and", pattern2Str +":")
-    result = hammingDistance(pattern1, pattern2)
-    print("Hamming distance is " + str(result) +".")
-    self.assertEqual(expectedResult, result)
-
-
-  def testReadB64File(self):
-    print("Testing readB64File()...")
-    pathToFile = os.path.dirname(os.path.realpath(__file__)) + "/set1/testReadB64.txt"
-    ba = readB64File(pathToFile)
-    print(ba)
-    expectedRes = "abcdefhH87"
-    self.assertEqual(expectedRes, ba.decode("ascii"))
-
-
-  def testSplitBytearray(self):
-    print("Testing testSplitBytearray()...")
-    ba = hexStrToByteArray("0xffaabb11cc99ee88dd")
-    expectedRes1 = [bytearray(b'\xff\xbb\xcc\xee\xdd'), bytearray(b'\xaa\x11\x99\x88')]
-    expectedRes2 = [bytearray(b'\xff\x11\xee'), bytearray(b'\xaa\xcc\x88'), bytearray(b'\xbb\x99\xdd')]
-    res1 = splitByteArray(ba, 2)
-    res2 = splitByteArray(ba, 3)
-    self.assertEqual(ba, splitByteArray(ba,1)[0])
-    self.assertEqual(expectedRes1, res1)
-    self.assertEqual(expectedRes2, res2)
-
-  def testDecryptXorText(self):
-    print(">>> Resolving set1/challenge6.txt")
-    path_to_challenge6 = os.path.dirname(os.path.realpath(__file__)) + "/set1/challenge6.txt"
-    cipher_text = readB64File(path_to_challenge6)
-    plain_text = decrypt_xor_text(cipher_text)
-    self.assertEqual("I'm back and", plain_text[0:12].decode("utf-8"))
-
-    print(">>> Resolving set1/my_plain_b64.txt")
-    path_to_my_plain = os.path.dirname(os.path.realpath(__file__)) + "/set1/my_plain_b64.txt"
-    my_key = bytearray("goodd", "ascii")
-    plain_text = readB64File(path_to_my_plain)
-    cipher_text = xorKey(plain_text, my_key)
-    plain_text_back = decrypt_xor_text(cipher_text)
-    self.assertEqual(plain_text, plain_text_back)
-
-
-if __name__ == "__main__" :
-  unittest.main()
+if __name__ == "__main__":
+    unittest.main()
